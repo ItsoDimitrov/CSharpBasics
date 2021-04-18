2c83fce3ec27198d9f5cc3e4176b881b2126b84f
ItsoDimitrov <fffsssccc@abv.bg>
2021-04-18 23:20:49 +0300

    Completed third exercise 'OpinionPoll'

--- OOPDefningClasses/03.OpinionPoll/OpinionPoll.cs
+++ OOPDefningClasses/03.OpinionPoll/OpinionPoll.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03.OpinionPoll
 {
@@ -6,7 +8,19 @@ namespace _03.OpinionPoll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            List<Person> result = new List<Person>();
+            for (int i = 1; i <= n; i++)
+            {
+                List<string> personInfo = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                Person person = new Person(personInfo[0], int.Parse(personInfo[1]));
+                result.Add(person);
+            }
+            Console.WriteLine();
+
+            result.Where(p => p.age > 30)
+                .OrderBy(p => p.name).ToList().ForEach(p => Console.WriteLine($"{p.name} - {p.age}"));
         }
     }
 }
---
+++ OOPDefningClasses/03.OpinionPoll/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.OpinionPoll
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
