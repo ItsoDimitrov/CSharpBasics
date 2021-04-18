fe437e9a60a4486a4bd4fcd855e6036986af5fc5
ItsoDimitrov <fffsssccc@abv.bg>
2021-04-18 22:41:48 +0300

    Creating second console app for second exercise 'Creating a constructor'

---
+++ OOPDefningClasses/02.CreatingAConstructor/CreatingAConstructor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02.CreatingAConstructor
+{
+    class CreatingAConstructor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
