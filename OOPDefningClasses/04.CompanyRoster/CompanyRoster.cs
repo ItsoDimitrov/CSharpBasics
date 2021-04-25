d10ce0975851df2aee6b462de81c13053e4dc6aa
ItsoDimitrov <fffsssccc@abv.bg>
2021-04-25 18:20:09 +0300

    Added new console application for exercise 04

---
+++ OOPDefningClasses/04.CompanyRoster/CompanyRoster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04.CompanyRoster
+{
+    class CompanyRoster
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
